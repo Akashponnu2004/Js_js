9bdef208bd61c7f3d2c7f63037399ae359d0660a
Akashponnu2004 <154038855+Akashponnu2004@users.noreply.github.com>
2026-07-29 14:22:26 +0000

    conversionofdatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score= "45ab"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+/* 
+"45" => 45
+"45ab"=> NaN
+true=>1 ; false=>0
+
+*/
+
+let isLoggedIn = 0
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
+
+// 1 =>true; 0 =>false
+// "" =>false
+//"hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber));
